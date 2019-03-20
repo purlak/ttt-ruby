13783e99c631de6d45135ebf02dd574173edadbf
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-20 16:44:39 -0400

    add menu and user input options

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,26 @@
 class TicTacToe
   def call
-    puts "Hello World"
+    system("clear")
+    puts "\n Welcome to Tic Tac Toe!"
+    menu
+    user_input
   end
+
+  def menu
+    puts "\n Here are your menu options:"
+    puts "1. Play Game"
+    puts "2. Exit"
+  end
+
+  def user_input
+    input = 0
+    while input
+      puts "Enter your option:"
+      input = gets.chomp
+      case (input)
+
+      end 
+    end
+  end
+
 end
