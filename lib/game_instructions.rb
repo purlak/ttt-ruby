84b8b35dae4bfbecddb3d7cca9186d0367fecdef
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-21 11:21:45 -0400

    add game instructions

--- lib/game_instructions.rb
+++ lib/game_instructions.rb
@@ -1,8 +1,21 @@
 class GameInstructions
 
   def call
-    puts "Hello World"
+    system("clear")
+    display_instructions
+    menu_options
   end
 
+  def display_instructions
+    puts "\n"
+    puts "Tic-tac-toe (American English), noughts and crosses (British English) or Xs and Os, is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game."
+    puts "Source: https://en.wikipedia.org/wiki/Tic-tac-toe"
+  end
+
+  def menu_options
+    puts "\n"
+    puts "To continue, select your option below:"
+    TicTacToe.new.menu
+  end
 
 end
