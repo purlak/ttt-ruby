279d4c0d0355ccafd368e86947b62ea450619ab8
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-21 11:56:22 -0400

    display empty board

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,16 @@
+
 class DisplayBoard
+  def initialize
+    @board = [" "," "," "," "," "," "," "," "," "]
+  end
 
   def call
-    puts "Hello World"
+    system("clear")
+    display_board(@board)
+  end
+
+  def display_board(board)
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
   end
 
 end
