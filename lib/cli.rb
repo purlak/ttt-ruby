c91ef80969e89dbae9269df2f32b2306a574675d
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-21 10:52:47 -0400

    update menu options

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,30 +1,37 @@
 class TicTacToe
   def call
     system("clear")
-    puts "\n Welcome to Tic Tac Toe!"
+    puts "Welcome to Tic Tac Toe!"
+    puts "\n"
     menu
     user_input
   end
 
   def menu
-    puts "\n Here are your menu options:"
+    puts "Here are your menu options:"
     puts "1. Play Game"
-    puts "2. Exit"
+    puts "2. Game Instructions"
+    puts "3. Exit"
+    puts "\n"
   end
 
   def user_input
     input = ""
     while input
       puts "Enter your option:"
-      input = gets.chomp
+      input = gets.strip
       case input
         when "1"
           display_board()
         when "2"
-          puts "Goodbye!"
+          game_instructions()
+        when "3"
+          puts "Goodbye! See you soon."
+          puts "\n"
           exit
         else
           puts "Invalid choice. Try again!"
+          puts "/n"
           menu
       end
     end
