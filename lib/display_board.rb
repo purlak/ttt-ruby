d915df5a8e0855d3a080aa0855535b1c99b5bd07
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-21 14:09:35 -0400

    add marker option

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,6 +11,13 @@ class DisplayBoard
 
   def display_board(board)
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+    choose_marker
   end
 
+  def choose_marker
+    input = ""
+    puts "Choose Marker (X or O)"
+    input = gets.strip
+    binding.pry
+  end
 end
