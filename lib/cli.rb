5452c7fda59466ed55bf54f63cdf928de95e20b9
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-22 17:00:09 -0400

    isolate code for testing

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,6 @@ class TicTacToe
     puts "Welcome to Tic Tac Toe!"
     puts "\n"
     menu
-    user_input
   end
 
   def menu
@@ -13,27 +12,5 @@ class TicTacToe
     puts "2. Game Instructions"
     puts "3. Exit"
     puts "\n"
-  end
-
-  def user_input
-    input = ""
-    while input
-      puts "Enter your option:"
-      input = gets.strip
-      case input
-        when "1"
-          DisplayBoard.new.call
-        when "2"
-          GameInstructions.new.call
-        when "3"
-          puts "Goodbye! See you soon."
-          puts "\n"
-          exit
-        else
-          puts "Invalid choice. Try again!"
-          puts "\n"
-          menu
-      end
-    end
   end
 end
