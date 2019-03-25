de1c7ed991207508d9b0c21d31cdc359c66c9c70
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-25 11:39:41 -0400

    add test to check if menu is called

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,4 +4,9 @@ describe 'Welcome' do
   it 'displays welcome message "Welcome to Tic Tac Toe!"' do
     expect { print 'Welcome to Tic Tac Toe!' }.to output('Welcome to Tic Tac Toe!').to_stdout
   end
+
+  it 'should call "menu"' do
+    new_menu = TicTacToe.new
+    expect(new_menu).to respond_to(:menu)
+  end
 end
