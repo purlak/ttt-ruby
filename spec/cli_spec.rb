7c51f98c75da2ad94670338976aac2a1ce08f63a
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-25 10:56:20 -0400

    add test for welcome message

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/cli.rb'
 
-describe 'cli' do
-
-
+describe 'Welcome' do
+  it 'displays welcome message "Welcome to Tic Tac Toe!"' do
+    expect { print 'Welcome to Tic Tac Toe!' }.to output('Welcome to Tic Tac Toe!').to_stdout
+  end
 end
