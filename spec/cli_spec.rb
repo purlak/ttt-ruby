14c884fd0a2a99d828c66878f02d5cfbaa1c49ad
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-26 16:14:10 -0400

    add before action" "
    "
    ""

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,14 +1,17 @@
 require_relative '../lib/cli.rb'
 
 describe 'call' do
+  before do
+    @cli = Cli.new
+  end
   it 'displays menu' do
-    cli = Cli.new
+    # cli = Cli.new
     expected =
       "Welcome to Tic Tac Toe!\n" +
       "Game Menu\n" +
       "1. Play Game\n" +
       "2. Game Instructions\n" +
       "3. Exit\n"
-    expect { cli.call }.to output(expected).to_stdout
+    expect { @cli.call }.to output(expected).to_stdout
   end
 end
