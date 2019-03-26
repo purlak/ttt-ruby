cc9de98ec849d82634d38ae9cae0458edbc592f4
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-26 11:46:47 -0400

    Build userinput method

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,6 +3,7 @@ class Cli
     system("clear")
     puts "Welcome to Tic Tac Toe!\n"
     puts menu
+    user_input
   end
 
   def menu
@@ -10,5 +11,21 @@ class Cli
     "1. Play Game\n" +
     "2. Game Instructions\n" +
     "3. Exit\n"
+  end
+
+  def user_input
+    input = ""
+    input = gets.strip
+    while (input)
+      case (input = '1')
+        #Game.rb
+      case (input = '2')
+        #GameInstructions.rb
+      case (input = '3')
+        #break
+      else
+        #puts "Invalid choice. Try again"
+      end
+    end
   end
 end
