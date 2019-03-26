b41f6cbd60eb957bc9ec4d4cab3fe432a956b741
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-03-26 11:47:55 -0400

    add test - call user_input

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -12,4 +12,9 @@ describe 'call' do
       "3. Exit\n"
     expect { cli.call }.to output(expected).to_stdout
   end
+
+  it 'calls user_input' do
+    cli = Cli.new
+    expect { cli.call }.to respond_to(:user_input)
+  end
 end
