73d2d091d1277860f8a7894c186a858e5b627c7b
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-04-08 11:09:45 -0400

    add tests to show menu and select marker

--- spec/content_spec.rb
+++ spec/content_spec.rb
@@ -1,6 +1,10 @@
 require_relative '../lib/content.rb'
-
 describe 'content' do 
+  it "shows menu" do 
+  	expect(Content.menu).to eq("Welcome to Tic Tac Toe!")
+	end
 
-
+	it "shows option to select marker" do
+		expect(Content.select_marker).to eq("Choose 'X' or 'O' to play game:")
+	end 
 end 
