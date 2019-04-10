bb651e10efe609b72399de8cc59782c816af2802
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-04-10 14:27:47 -0400

    remove test related to user input and game

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -10,21 +10,15 @@ describe 'call' do
 
   it 'clears screen before printing menu' do
     expect(@cli).to receive(:clear_screen)
-    allow(@cli).to receive(:gets).and_return(' ')
+    allow(@cli).to receive(:gets).and_return('')
     @cli.call
   end
 
   it 'displays welcome message and marker options' do
     allow(@cli).to receive(:clear_screen)
-    allow(@cli).to receive(:gets).and_return(' ')
+    allow(@cli).to receive(:gets).and_return('')
     expect(@cli).to receive(:display_text).with(Content.welcome)
     expect(@cli).to receive(:display_text).with(Content.select_marker)
-    @cli.call
-  end
-
-  it 'calls user input' do
-    allow(@cli).to receive(:clear_screen)
-    expect(@cli).to receive(:user_input)
     @cli.call
   end
 end
@@ -38,26 +32,5 @@ describe 'display_text' do
     text = 'test'
     expect(@cli).to receive(:display_text).with(text).and_return(text)
     @cli.display_text(text)
-  end
-end
-
-describe 'user_input' do
-  before do
-    allow($stdin).to receive(:gets)
-    allow($stdout).to receive(:puts)
-    @cli = Cli.new
-  end
-
-  it 'takes an user input' do
-    allow(@cli).to receive(:gets).and_return('X')
-    @cli.user_input
-  end
-
-  it 'starts a new game' do
-    allow(@cli).to receive(:gets).and_return(' ')
-    game = Game.new
-    expected = "  |  |  \n--------\n  |  |  \n--------\n  |  |  \n"
-    expect(@cli).to receive(:user_input).and_return(expected)
-    @cli.user_input
   end
 end
