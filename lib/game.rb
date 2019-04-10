419cb4933d9da60a8fcd82b535700caf5fd638f5
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-04-10 14:43:41 -0400

    add user_input method to game loop and tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,17 @@
 class Game
   attr_accessor :board
 
+  def call
+    user_input
+    display_board
+  end
+
   def initialize
     @board = Array.new(9)
+  end
+
+  def user_input
+    gets.chomp
   end
 
   def display_board
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,8 @@ require_relative '../lib/game.rb'
 
 describe 'game' do
   before do
+    allow($stdin).to receive(:gets)
+    allow($stdout).to receive(:puts)
     @game = Game.new
   end
 
@@ -9,10 +11,23 @@ describe 'game' do
     expect(@game.instance_variable_get(:@board)).to eq(Array.new(9))
   end
 
-  it 'shows an empty board' do
-    allow($stdout).to receive(:puts)
-    expected = "  |  |  \n--------\n  |  |  \n--------\n  |  |  \n"
-    expect { @game.display_board }.to output(expected).to_stdout
-    @game.display_board
+  describe 'call' do
+    it 'calls user input' do
+      expect(@game).to receive(:user_input)
+      @game.call
+    end
+
+    it 'shows an empty board' do
+      allow($stdout).to receive(:puts)
+      expected = "  |  |  \n--------\n  |  |  \n--------\n  |  |  \n"
+      expect { @game.display_board }.to output(expected).to_stdout
+      @game.display_board
+    end
   end
+
+  describe 'user_input' do
+    it 'asks the user for input' do
+      allow(@game).to receive(:gets).and_return('X')
+      @game.user_input
+    end
 end
