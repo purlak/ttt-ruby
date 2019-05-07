dbe3ee1eb0466bc4c54abf64fc1e06c256a81a64
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-05-07 12:59:06 -0400

    extract rules into a module

---
+++ lib/game_rules.rb
@@ -0,0 +1,32 @@
+module GameRules
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ].freeze
+
+  CORNERS = [0, 2, 8, 6].freeze
+
+  def won?(board, current_player)
+    WIN_COMBINATIONS.find do |combo|
+      board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] != ' '
+    end
+  end
+
+  def draw?(board)
+    board.full?(board.cells) && !won?(board, current_player)
+  end
+
+  def over?(board)
+    won?(board, current_player) || draw?(board)
+  end
+
+  def winner(board)
+    board.cells[won?(board, current_player)[0]] if won?(board,current_player)
+  end
+end
