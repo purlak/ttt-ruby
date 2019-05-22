7ade1d60ab9f5e6aba25310ae7661e9d2c9f46c8
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-05-21 22:22:23 -0400

    add test to check if ai v. ai game draws

--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/game'
+require_relative '../lib/game_rules'
 require_relative '../lib/board'
 require_relative '../lib/players/human'
 require_relative '../lib/players/ai'
@@ -108,6 +109,25 @@ describe Minimax do
         ]
 
         expect(Minimax.new.move(@board, @player_1, @player_2)).to eq(4)
+      end
+    end
+
+    context 'Ai plays against Ai' do
+      before do
+        @player_1 = Player::Ai.new('X')
+        @player_2 = Player::Ai.new('O')
+      end
+
+      it 'Game results in a draw' do
+        @board.cells = [
+          'O', 'X', 'X',
+          'X', 'X', 'O',
+          'O', 'O', ' '
+        ]
+
+        expect(Minimax.new.move(@board, @player_1, @player_2)).to eq(9)
+        @board.update_board(9, @player_1)
+        expect(GameRules.draw?(@board)). to eq(true)
       end
     end
   end
