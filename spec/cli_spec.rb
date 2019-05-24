3cf371adad348927988984c3d077c5d2bfbdcfad
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-05-24 15:00:31 -0400

    update cli test

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/cli.rb'
 require_relative '../lib/content.rb'
+require_relative '../lib/display_text.rb'
 
 describe Cli do
   before do
@@ -10,7 +11,7 @@ describe Cli do
     it 'clears screen before printing menu' do
       expect(subject).to receive(:clear_screen)
 
-      subject.call
+      subject.call(DisplayText.method(:display_text))
     end
 
     it 'displays welcome message' do
@@ -18,7 +19,7 @@ describe Cli do
       expected = "Welcome to Tic Tac Toe!\n\n"
 
       expect do
-        subject.call
+        subject.call(m = DisplayText.method(:display_text))
       end.to output(expected).to_stdout
     end
   end
