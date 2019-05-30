58ee76c6d645e6e164e051da49080875417ccb53
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-05-30 10:14:41 -0400

    update get input

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,6 +2,6 @@ require_relative '../get_input.rb'
 class Player::Human < Player
   def move(_board, _player1, _player2)
     puts 'Enter board position [1-9]:'
-    GetInput.call
+    GetInput.new.call
   end
 end
